6a80b822022419e7e37b77561eb25dcde3f61ffc
github username <mdam2345@gmail.com>
2019-03-06 14:50:53 -0500

    User update with show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-
+    @users = User.all
   end
 
   def new
@@ -25,11 +25,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update(user_params)
        flash[:success] = "Your account was updated successfully"
-       redirect_to root
+       redirect_to users_path
     else
       render 'edit'
     end
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
 
   private
 
