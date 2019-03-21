7318bdc91d2e7e027c6c92984dd4a6d1d1afaeb4
MDAM182 <mdam2345@gmail.com>
2019-03-20 20:37:02 -0400

    updated workout_category model

--- app/models/workout_category.rb
+++ app/models/workout_category.rb
@@ -1,4 +1,10 @@
 class WorkoutCategory < ApplicationRecord
   belongs_to :workout
   belongs_to :category
+  scope :beginner, -> { where(level: 'Beginner') }
+  scope :intermediate, -> { where(level: 'Intermediate') }
+  scope :advanced, -> { where(level: 'Advanced') }
+  scope :expert, -> { where(level: 'Expert') }
+
+  validates :level, presence: true
 end
